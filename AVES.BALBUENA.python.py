af1f84e89d52ac04f705dc9711fc09c57cd5f11e
Alumno <Alumno@PC-B02>
2023-10-10 10:46:07 -0600

    Agregar archivos de proyecto.

---
+++ AVES.BALBUENA.python.py
@@ -0,0 +1,24 @@
+#aves.balbuena
+#declara una lista de cadenas
+aves=["loro gris","paloma de diamante","coctel"]
+print("los valores de la lista antes de insertar")
+for ave in aves:
+    print(ave,end="")
+
+    print("\n")
+
+
+    
+aves.append("mayna")
+aves.append("periquitos")
+aves.append("cacatua")
+
+print("los valores de la lista despues de insertar:")
+
+for aves in aves:
+
+    print(aves,end="")
+
+
+    print("\n")
+
